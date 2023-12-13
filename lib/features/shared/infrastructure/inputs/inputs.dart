adaf37b50d75c1975d871af9aacc2a710cf91c87
Your Name <antoniojz1998@gmail.com>
2023-12-13 18:46:34 +0100

    edit/create form inputs

--- lib/features/shared/infrastructure/inputs/inputs.dart
+++ lib/features/shared/infrastructure/inputs/inputs.dart
@@ -2,3 +2,7 @@ export 'email_input.dart';
 export 'password_input.dart';
 export 'confirm_password_input.dart';
 export 'fullname_input.dart';
+export 'slug_input.dart';
+export 'price_input.dart';
+export 'stock_input.dart';
+export 'title_input.dart';
---
+++ lib/features/shared/infrastructure/inputs/stock_input.dart
@@ -0,0 +1,35 @@
+import 'package:formz/formz.dart';
+
+// Define input validation errors
+enum StockError { empty, negative, format }
+
+// Extend FormzInput and provide the input type and error type.
+class Stock extends FormzInput<int, StockError> {
+  // Call super.pure to represent an unmodified form input.
+  const Stock.pure() : super.pure(0);
+
+  // Call super.dirty to represent a modified form input.
+  const Stock.dirty(int value) : super.dirty(value);
+
+  String? get errorMessage {
+    if (isValid || isPure) return null;
+
+    if (displayError == StockError.empty) return 'This field is required';
+    if (displayError == StockError.negative) return 'Can´t be less than 0';
+
+    return null;
+  }
+
+  // Override validator to handle validating a given input value.
+  @override
+  StockError? validator(int value) {
+    if (value.toString().isEmpty || value.toString().trim().isEmpty) {
+      return StockError.empty;
+    }
+
+    if ((int.tryParse(value.toString()) ?? -1) == -1) return StockError.format;
+
+    if (value < 0) return StockError.negative;
+    return null;
+  }
+}
