05d9c3fbf21cea02749db1d7becee29b48d06c5a
Your Name <antoniojz1998@gmail.com>
2023-12-13 02:13:10 +0100

    data infrastructure layer

---
+++ lib/features/products/infrastructure/repositories/products_repository_impl.dart
@@ -0,0 +1,27 @@
+import 'package:teslo_shop/features/products/domain/domain.dart';
+
+class ProductsRepositoryImpl extends ProductsRepository {
+  final ProductsDatasource productsDatasource;
+
+  ProductsRepositoryImpl(this.productsDatasource);
+
+  @override
+  Future<Product> createUpdateProduct(Map<String, dynamic> product) {
+    return productsDatasource.createUpdateProduct(product);
+  }
+
+  @override
+  Future<Product> getProductById(String id) {
+    return productsDatasource.getProductById(id);
+  }
+
+  @override
+  Future<List<Product>> getProductByPage({int limit = 10, offset = 0}) {
+    return productsDatasource.getProductByPage(limit: limit, offset: offset);
+  }
+
+  @override
+  Future<List<Product>> searchProductByParam(String query) {
+    return productsDatasource.searchProductByParam(query);
+  }
+}
