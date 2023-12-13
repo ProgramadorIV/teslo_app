74ab6a81bc0568486abf6f046907fb820c0af392
Your Name <antoniojz1998@gmail.com>
2023-12-13 03:20:30 +0100

    image slider widget

---
+++ lib/features/products/presentation/widgets/product_card.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:teslo_shop/features/products/domain/domain.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    super.key,
+    required this.product,
+  });
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _ImageSlider(images: product.images),
+        const SizedBox(height: 5),
+        Text(
+          product.title,
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+}
+
+class _ImageSlider extends StatelessWidget {
+  const _ImageSlider({required this.images});
+
+  final List<String> images;
+
+  @override
+  Widget build(BuildContext context) {
+    if (images.isEmpty) {
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Image.asset(
+          '/assets/images/no-image.jpg',
+          fit: BoxFit.cover,
+          height: 250,
+        ),
+      );
+    }
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: FadeInImage(
+        fadeOutDuration: const Duration(milliseconds: 300),
+        fadeInDuration: const Duration(milliseconds: 300),
+        placeholder: const AssetImage('assets/loaders/bottle-loader.gif'),
+        image: NetworkImage(images.first),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
