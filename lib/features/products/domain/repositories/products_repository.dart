f913dec6c93edeffe4e484e42d9f87f0da44ca61
Your Name <antoniojz1998@gmail.com>
2023-12-13 02:12:03 +0100

    domain layer

---
+++ lib/features/products/domain/repositories/products_repository.dart
@@ -0,0 +1,8 @@
+import '../entities/products.dart';
+
+abstract class ProductsRepository {
+  Future<List<Product>> getProductByPage({int limit = 10, offset = 0});
+  Future<Product> getProductById(String id);
+  Future<List<Product>> searchProductByParam(String query);
+  Future<Product> createUpdateProduct(Map<String, dynamic> product);
+}
